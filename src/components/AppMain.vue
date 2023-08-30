c626dacdf1fcba0bcae9ff58a6f797018ac5311f
DanielMarella <marella.daniel96@gmail.com>
2023-08-30 17:09:47 +0200

    added my api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,51 @@
 <template>
     <div>
-        <h1>
-            MAIN
-        </h1>
+        <p v-for="project in projects">
+            {{ project.title }}
+        </p>
     </div>
 </template>
+
 <script>
+
+import axios from 'axios';
+
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+
+    data() {
+        return {
+            projects : [],
+            apiUrl : 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+
+    methods: {
+        getProjects(){
+
+            axios.get(this.apiUrl, {
+                params: {}
+            })
+            .then((response)=> {
+                console.log(response.data.results);
+                this.projects = response.data.results;
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+        },
+
+    created(){
+        this.getProjects();
+    },
+
+    },
+
 }
 </script>
+
+
 <style lang="scss">
     
 </style>
