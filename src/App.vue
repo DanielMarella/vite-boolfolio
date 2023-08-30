be1175f0cb052a4ebc6551cac7f672776e55aa29
DanielMarella <marella.daniel96@gmail.com>
2023-08-30 16:47:44 +0200

    import my new component to the main App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <script>
 import AppMain from './components/AppMain.vue'
-
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 export default{
   name: 'App',
 
   components:{
-    AppMain
+    AppHeader,
+    AppMain,
+    AppFooter
   }
 }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <AppMain />
 
+<header>
+  <AppHeader />
+</header>
+
+<main>
+  <AppMain />  
+</main>
+
+<footer>
+  <AppFooter />
+</footer>
 </template>
 
 
