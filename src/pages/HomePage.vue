27216db9b3a12fa747e9059dd9853eeb8871c0c6
DanielMarella <marella.daniel96@gmail.com>
2023-08-31 16:02:40 +0200

    structuring with some html my pages component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        
+        <h1>Homepage</h1>
     </div>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quis dolore sapiente! Mollitia accusantium praesentium eius velit vel tempore illo? Incidunt nam veritatis autem nemo odio vero eligendi suscipit tenetur!
+    </p>
 </template>
 
 <script>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        
+        <h1>
+            Projects
+        </h1>
     </div>
 </template>
 
