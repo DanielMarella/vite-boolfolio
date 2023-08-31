3029e72d9cb7b865ae6b2c017661fa7a27ad14ff
DanielMarella <marella.daniel96@gmail.com>
2023-08-31 16:02:17 +0200

    added some html and style for better visualizzation on page of the nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-    </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Projects
+                    </a>
+                </li>
+            </ul>
+        </nav>
 </template>
 <script>
 export default {
@@ -8,5 +20,30 @@ export default {
 }
 </script>
 <style lang="scss">
-    
+    nav{
+        width: 100%;
+        padding: 1rem 3rem;
+        background-color: grey;
+        color: white;
+
+        ul{
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            list-style-type: none;
+            font-size: 2rem;
+
+            li{
+                margin-right: 2rem;
+
+                a{
+                    text-decoration: none;
+                    color: black;
+                }
+
+            }
+        }
+    }
+
+
 </style>
